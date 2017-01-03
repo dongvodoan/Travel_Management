5ca5a1fbfc9faec936a569ad938625d1ab4e3857
ledinhvu <dinhvu12t3bkdn@gmail.com>
2017-01-03 15:22:19 +0700

    edit category tours

--- app/Models/CategoryTour.php
+++ app/Models/CategoryTour.php
@@ -41,7 +41,8 @@ class CategoryTour extends Model
      * @var array
      */
     public static $rules = [
-        'name' => 'required'
+        'name' => 'required|max:30|min:3',
+        'describe' => 'required'
     ];
 
     /**
